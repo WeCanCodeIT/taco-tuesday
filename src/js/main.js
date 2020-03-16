8ec2cd39c79aac1cfa051333a5fdc5187db839a9
Benjamin Williams <ben@wecancodeit.org>
2020-03-16 11:01:16 -0400

    Making tacos appear in the console.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
-fetch('http://taco-randomizer.herokuapp.com/random/')
-.then(response => response.json())
-.then(json=> console.log(json))
+const tacoButton = document.querySelector(".taco-button");
+
+const getARandomTaco= () =>{
+    fetch('http://taco-randomizer.herokuapp.com/random/')
+    .then(response => response.json())
+    .then(taco => console.log(taco));
+}
+
+tacoButton.addEventListener('click', getARandomTaco);
+
+
+
+// fetch('http://taco-randomizer.herokuapp.com/random/')
+// .then(response => response.json())
+// .then(json=> console.log(json))
